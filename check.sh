98f5e93c7d3a93f2b19ca92cfacd28275fd0d994
root <75710541@qq.com>
2016-10-14 21:14:18 +0800

    check.sh

---
+++ check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function check_port(){
+	name=$1
+	port=$2
+	netstat -lntp|grep $port &>/dev/null&& echo "$name is ok" || (echo "$name is error";exit 1)
+}
+
+check_port grafana 3000
+check_port prometheus 9090
+
+
+
+
+
